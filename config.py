3e782ae23282bb6e48affcbcbf15425f69c7b209
kumajimmy <107084578+kumajimmy@users.noreply.github.com>
2024-05-10 12:35:45 -0700

    sixth commit added config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+#firebase db containing the mapping data
+firebase_url = "https://mushroom-poison-default-rtdb.firebaseio.com/.json"
+
+
+
+DATABASE_URLS = {
+    0: "https://mushroom-poison-0.firebaseio.com/",
+    1: "https://mushroom-poison-1.firebaseio.com/",
+    2: "https://mushroom-poison-2.firebaseio.com/",
+    3: "https://mushroom-poison-3.firebaseio.com/",
+    4: "https://mushroom-poison-4.firebaseio.com/",
+    5: "https://mushroom-poison-5.firebaseio.com/",
+    6: "https://mushroom-poison-6.firebaseio.com/",
+    7: "https://mushroom-poison-7.firebaseio.com/",
+}
